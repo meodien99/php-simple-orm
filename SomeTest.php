010c41fb6f458ea082fdbe93a3cdb3d74c031260
meodien99 <seb.madcat@gmail.com>
2014-11-26 16:04:40 +0700

    init php unit test

---
+++ SomeTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: madcat
+ * Date: 11/26/14
+ * Time: 4:00 PM
+ */
+
+class SomeTest extends PHPUnit_Framework_TestCase{
+
+    public function testSomeThing(){
+        $this->assertTrue(true);
+    }
+} 
