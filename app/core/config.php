dca1b546cdde4789b1c36efa5a6c9510d4885090
meodien99 <seb.madcat@gmail.com>
2014-11-26 17:23:26 +0700

    init db file

---
+++ app/core/config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'mysql' => [
+        'host' => 'localhost',
+        'dbname' => 'orm',
+        'username' => 'root',
+        'password' => 'root'
+    ],
+    'sqlite' => 'sqlite::memory'
+];
