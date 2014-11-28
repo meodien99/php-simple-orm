f113f08258c72f08b4bd9d70dc1d73e316e9dc85
meodien99 <seb.madcat@gmail.com>
2014-11-28 16:15:17 +0700

    complete DB file

--- src/ORM/DB/Database.php
+++ src/ORM/DB/Database.php
@@ -1,5 +1,5 @@
 <?php
-namespace app\ORM\DB;
+namespace ORM\DB;
 
 use PDO;
 class Database {
@@ -7,6 +7,7 @@ class Database {
     public static $DB;
 
     private $_db_engine = 'mysql';
+    private $_db_connect;
     private $_error;
     private $_statement;
 
@@ -17,18 +18,20 @@ class Database {
         if(!is_null($config)){
             try {
                 if($this->_db_engine == 'mysql'){
-                    $dns = "mysql::host=" . $config['host'] . ";dbname=" . $config['db'];
-                    self::$DB = new PDO($dns, $config['user'], $config['pass']);
+                    $dns = "mysql:host=" . $config['host'] . ";dbname=" . $config['db'];
+                    $this->_db_connect = new PDO($dns, $config['user'], $config['pass']);
                 } else if ($this->_db_engine == 'sqlite'){
-                    self::$DB = new PDO($config['memory']);
+                    $this->_db_connect = new PDO($config['memory']);
                 }
-                    self::$DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->_db_connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //                    self::$DB->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
             } catch (\PDOException $e) {
                 echo "Connection Error: " . $e->getMessage();
             }
         }
     }
+
+    private function __clone(){}
 
     private function dataConfig($engine){
         $data = require_once(__DIR__.'/../config.php');
@@ -44,8 +47,8 @@ class Database {
         return self::$DB;
     }
 
-    public function prepare($query){
-        $this->_statement = self::$DB->prepare($query);
+    public function prepares($query){
+        $this->_statement = $this->_db_connect->prepare($query);
     }
 
     public function bind($params, $value, $type = null){
@@ -77,7 +80,9 @@ class Database {
                 .($limit ? "LIMIT {$limit} " : '' )
                 .(($offset && $limit) ? "OFFSET {$offset} ": '')
                 .($order ? "ORDER BY {$order} " : '');
-        $this->prepare($query);
+
+        $this->prepares($query);
+
     }
 
     public function insert($table, array $data){
@@ -85,9 +90,9 @@ class Database {
 
         $fieldNames = implode(',', array_keys($data));
         $fieldValues = ':'.implode(", :", array_keys($data));
+        $query = "INSERT INTO {$table}({$fieldNames}) VALUES({$fieldValues})";
 
-        $query = "INSERT INTO {$table} ({$$fieldNames}) VALUES({$$fieldValues})";
-        $this->prepare($query);
+        $this->prepares($query);
 
         foreach($data as $key=>$value){
             $this->bind(":$key", $value);
@@ -104,7 +109,7 @@ class Database {
         $fieldDetails = rtrim($fieldDetails, ",");
 
         $query = "UPDATE {$table} SET {$fieldDetails} ". ($where ? "WHERE {$where}": ' ');
-        $this->prepare($query);
+        $this->prepares($query);
 
         foreach ($data as $key => $value){
             $this->bind(":$key", $value);
@@ -114,7 +119,7 @@ class Database {
     }
 
     public function delete($table, $where, $limit = 1) {
-        $this->prepare("DELETE FROM {$table} WHERE {$where} LIMIT $limit");
+        $this->prepares("DELETE FROM {$table} WHERE {$where} LIMIT $limit");
         $this->execute();
     }
 
@@ -139,6 +144,7 @@ class Database {
     public function objectSingle($class, $ctorargs = [] ) {
         $this->execute();
         $this->_statement->setFetchMode(PDO::FETCH_CLASS, $class, $ctorargs);
+
         return $this->_statement->fetch();
     }
 
