aad74eee58d4b7b2fa9a8353df5d8906af0971fa
meodien99 <seb.madcat@gmail.com>
2014-11-29 10:20:53 +0700

    change to sqlite driven

--- src/ORM/config.php
+++ src/ORM/config.php
@@ -10,7 +10,7 @@ return [
         ],
         'sqlite' => [
             'memory' => 'sqlite::memory',
-            'file'   => 'sqlite::database.sqlite'
+            'file'   => 'sqlite:database.sqlite'
         ]
     ],
 ];
